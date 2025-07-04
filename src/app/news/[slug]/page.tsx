65625891a2164dcaaff24287ea40c713124d237d
raihanhykl <raihanhaikal40@gmail.com>
2025-07-04 18:10:35 +0700

    change to dynamic

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -2,28 +2,30 @@
 
 export const revalidate = 60;
 
-import { getAllNews } from "@/service/get-news";
+// import { getAllNews } from "@/service/get-news";
 import NewsClient from "./NewsClient";
 
-export async function generateStaticParams() {
-  const newsList = await getAllNews();
+// export async function generateStaticParams() {
+//   const newsList = await getAllNews();
 
-  if (!newsList || !Array.isArray(newsList.data)) {
-    return [];
-  }
+//   if (!newsList || !Array.isArray(newsList.data)) {
+//     return [];
+//   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return newsList.data.map((item: any) => ({
-    slug: item.slug,
-  }));
-}
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   return newsList.data.map((item: any) => ({
+//     slug: item.slug,
+//   }));
+// }
+
+export const dynamic = "force-dynamic";
 
 export default async function NewsDetailPage({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: { slug: string };
 }) {
-  const { slug } = await params;
+  const { slug } = params;
 
   return <NewsClient slug={slug} />;
 }
