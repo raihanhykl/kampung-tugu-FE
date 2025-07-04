f35bc89d45ea271707ced45560e3671b58d68d3b
raihanhykl <raihanhaikal40@gmail.com>
2025-07-04 17:30:35 +0700

    edit news slug

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,11 +1,17 @@
 // app/news/[slug]/page.tsx
+
 export const revalidate = 60;
 
-import { getAllNews } from "@/service/get-news"; // sesuaikan dengan data kamu
+import { getAllNews } from "@/service/get-news";
 import NewsClient from "./NewsClient";
 
 export async function generateStaticParams() {
   const newsList = await getAllNews();
+
+  if (!newsList || !Array.isArray(newsList.data)) {
+    return [];
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return newsList.data.map((item: any) => ({
     slug: item.slug,
@@ -15,9 +21,9 @@ export async function generateStaticParams() {
 export default async function NewsDetailPage({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: { slug: string };
 }) {
-  const { slug } = await params;
+  const { slug } = params;
 
   return <NewsClient slug={slug} />;
 }
