0901736b9f7299faf03328734a16251a2d384a23
raihanhykl <raihanhaikal40@gmail.com>
2025-07-09 22:46:54 +0700

    fix metadata for landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { getSEOMetadata } from "@/lib/seo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,11 +14,30 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+// export const metadata1: Metadata = {
+//   title: "Kampung Tugu Official Website",
+//   description:
+//     "Kampung Tugu adalah sebuah kawasan bersejarah yang terletak di Jakarta Utara, dikenal sebagai permukiman keturunan Portugis tertua di Indonesia. Didirikan pada abad ke-17 oleh para budak Kristen asal Malaka yang dibebaskan oleh VOC, mereka kemudian menetap di wilayah ini dan membentuk komunitas unik yang dikenal sebagai Orang Tugu. Jejak sejarah ini masih hidup dalam budaya, arsitektur, dan tradisi yang diwariskan turun-temurun.",
+// };
+
+export const metadata = getSEOMetadata({
   title: "Kampung Tugu Official Website",
   description:
     "Kampung Tugu adalah sebuah kawasan bersejarah yang terletak di Jakarta Utara, dikenal sebagai permukiman keturunan Portugis tertua di Indonesia. Didirikan pada abad ke-17 oleh para budak Kristen asal Malaka yang dibebaskan oleh VOC, mereka kemudian menetap di wilayah ini dan membentuk komunitas unik yang dikenal sebagai Orang Tugu. Jejak sejarah ini masih hidup dalam budaya, arsitektur, dan tradisi yang diwariskan turun-temurun.",
-};
+  keywords: [
+    "kampung tugu",
+    "produk desa",
+    "budaya lokal",
+    "tradisi",
+    "kerajinan tangan",
+    "produk khas",
+    "desa bersejarah",
+    "Indonesia",
+  ],
+  url: "https://kampungtugu.site",
+  image: "https://kampungtugu.site/images/hero.png",
+  lang: "id",
+});
 
 export default function RootLayout({
   children,
